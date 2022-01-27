99c34d3dd227d5dd82a305783b7f18b8fabf4e7f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-01-26 23:29:27 -0800

    Concurrent execution of t-SNE, clustering and UMAP.

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,18 @@
 #include <map>
 #include <memory>
 
+#include <omp.h>
+
 int main(int argc, char* argv []) {
     /** Parsing the arguments. **/
     CLI::App app{"Single-cell RNA-seq analyses on the command-line"};
     
     std::string path;
     app.add_option("path", path, "Path to the Matrix Market file")->required();
+
+    double nthreads;
+    app.add_option("-t,--nthreads", nthreads, "Number of threads to use (+2 for UMAP and t-SNE, which use their own threads)")
+        ->default_val(1);
 
     double nmads;
     app.add_option("--qc-nmads", nmads, "Number of MADs to use for filtering")
@@ -53,6 +59,10 @@ int main(int argc, char* argv []) {
         ->transform(CLI::CheckedTransformer(scheme_map, CLI::ignore_case))
         ->default_val(scran::BuildSNNGraph::RANKED);
 
+    double snn_res;
+    app.add_option("--snn-res", snn_res, "Resolution to use in multi-level community detection")
+        ->default_val(1);
+
     double tsne_perplexity;
     app.add_option("--tsne-perplexity", tsne_perplexity, "Perplexity to use in t-SNE")
         ->default_val(30);
@@ -74,6 +84,8 @@ int main(int argc, char* argv []) {
         ->default_val(500);
 
     CLI11_PARSE(app, argc, argv);
+
+    omp_set_num_threads(nthreads);
 
     // Loading the data from an MatrixMarket file.
     size_t n = path.size();
@@ -109,19 +121,58 @@ int main(int argc, char* argv []) {
         ptr.reset(new knncolle::VpTreeEuclidean<>(npcs, pca_res.pcs.cols(), pca_res.pcs.data()));
     }
 
-    // Performing clustering.
-    auto clust_res = scran::ClusterSNNGraphMultiLevel().set_weighting_scheme(snn_scheme).set_neighbors(snn_neighbors).run(ptr.get());
-    const auto& best_clustering = clust_res.membership[clust_res.max];
+    // Finding all the neighbors.
+    std::vector<std::vector<int> > snn_nns(ptr->nobs());
+    #pragma omp parallel for
+    for (size_t i = 0; i < snn_nns.size(); ++i) {
+        auto current = ptr->find_nearest_neighbors(i, snn_neighbors);
+        for (const auto& x : current) {
+            snn_nns[i].push_back(x.first);
+        }
+    }
 
-    // Throw in some marker detection.
-    auto marker_res = scran::ScoreMarkers().run(normalized.get(), best_clustering.data());
+    std::vector<std::vector<std::pair<int, double> > > tsne_nns(ptr->nobs());
+    {
+        int tsne_k = std::ceil(tsne_perplexity *3); // TODO: replace with thing.
+        #pragma omp parallel for
+        for (size_t i = 0; i < tsne_nns.size(); ++i) {
+            tsne_nns[i] = ptr->find_nearest_neighbors(i, tsne_k);
+        }
+    }
 
-    // Performing the t-SNE and UMAP.
-    std::vector<double> tsne_output(ptr->nobs() * 2);
-    qdtsne::Tsne<>().set_perplexity(tsne_perplexity).set_max_iter(tsne_iterations).run(ptr.get(), tsne_output.data());
+    std::vector<std::vector<std::pair<int, float> > > umap_nns(ptr->nobs());
+    #pragma omp parallel for
+    for (size_t i = 0; i < umap_nns.size(); ++i) {
+        auto current = ptr->find_nearest_neighbors(i, umap_neighbors);
+        for (const auto& x : current) {
+            umap_nns[i].emplace_back(x.first, x.second);
+        }
+    }
 
-    std::vector<double> umap_output(ptr->nobs() * 2);
-    umappp::Umap<>().set_num_neighbors(umap_neighbors).set_min_dist(umap_mindist).set_num_epochs(umap_epochs).run(ptr.get(), 2, umap_output.data());
+    std::vector<double> tsne_output = qdtsne::initialize_random<>(ptr->nobs());
+    std::vector<float> umap_output(ptr->nobs() * 2);
+
+    omp_set_nested(1);
+    #pragma omp parallel num_threads(3)
+    {
+        if (omp_get_thread_num() == 0) {
+            omp_set_num_threads(nthreads);
+
+            // Performing clustering.
+            auto snn_graph = scran::BuildSNNGraph().set_weighting_scheme(snn_scheme).run(snn_nns);
+            auto clust_res = scran::ClusterSNNGraphMultiLevel().set_resolution(snn_res).run(snn_nns.size(), snn_graph);
+            const auto& best_clustering = clust_res.membership[clust_res.max];
+
+            // Throw in some marker detection.
+            auto marker_res = scran::ScoreMarkers().run(normalized.get(), best_clustering.data());
+        } else if (omp_get_thread_num() == 1) {
+            omp_set_num_threads(1);
+            qdtsne::Tsne<>().set_perplexity(tsne_perplexity).set_max_iter(tsne_iterations).run(tsne_nns, tsne_output.data());
+        } else {
+            omp_set_num_threads(1);
+            umappp::Umap<float>().set_min_dist(umap_mindist).set_num_epochs(umap_epochs).run(umap_nns, 2, umap_output.data());
+        }
+    }
 
     return 0;
 }
