8fc6db50d4f185ad0cdccb42ddcdce98c72668a2
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-01-27 20:45:15 -0800

    Report timings as the analysis runs.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <map>
 #include <memory>
 #include <thread>
+#include <iostream>
+#include <chrono>
 
 int nthreads_global = 1;
 
@@ -107,9 +109,17 @@ int main(int argc, char* argv []) {
 
     CLI11_PARSE(app, argc, argv);
 
+    // Setting up some bits and pieces.
     nthreads_global = nthreads;
 
+    auto declare = [&](const auto& x) -> void {
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(end - x).count())/1000 << "s" << std::endl;
+    };
+
     // Loading the data from an MatrixMarket file.
+    std::cout << "Initializing matrix... " << std::flush;
+    auto start = std::chrono::high_resolution_clock::now();
     size_t n = path.size();
     tatami::LayeredMatrixData<double, int> mat;
     if (n > 3 && path[n-3] == '.' && path[n-2] == 'g' && path[n-1] == 'z') {
@@ -117,36 +127,71 @@ int main(int argc, char* argv []) {
     } else {
         mat = tatami::MatrixMarket::load_layered_sparse_matrix(path.c_str());
     }
+    declare(start);
 
     // Filtering out low-quality cells. 
+    std::cout << "Computing QC metrics... " << std::flush;
+    start = std::chrono::high_resolution_clock::now();
     auto qc_res = scran::PerCellQCMetrics().run(mat.matrix.get(), { /* mito subset definitions go here */ });
+    declare(start);
+
+    std::cout << "Computing QC thresholds... " << std::flush;
+    start = std::chrono::high_resolution_clock::now();
     auto qc_filters = scran::PerCellQCFilters().set_nmads(nmads).run(qc_res);
+    declare(start);
+
+    std::cout << "Filtering cells... " << std::flush;
+    start = std::chrono::high_resolution_clock::now();
     auto filtered = scran::FilterCells().run(mat.matrix, qc_filters.overall_filter.data());
+    declare(start);
 
     // Computing log-normalized expression values, re-using the total count from the QC step.
+    std::cout << "Log-normalizing the counts... " << std::flush;
+    start = std::chrono::high_resolution_clock::now();
     auto size_factors = scran::subset_vector<false>(qc_res.sums, qc_filters.overall_filter.data());
     auto normalized = scran::LogNormCounts().run(filtered, std::move(size_factors));
+    declare(start);
 
     // Identifying highly variable genes.
+    std::cout << "Mean-variance modelling... " << std::flush;
+    start = std::chrono::high_resolution_clock::now();
     auto var_res = scran::ModelGeneVar().set_span(span).run(normalized.get());
     auto keep = scran::ChooseHVGs().set_top(nhvgs).run(var_res.residuals.size(), var_res.residuals.data());
+    declare(start);
 
     // Performing a PCA on the HVGs. We transpose the output so cells are columns again.
+    std::cout << "Principal components analysis... " << std::flush;
+    start = std::chrono::high_resolution_clock::now();
     auto pca_res = scran::RunPCA().set_rank(npcs).run(normalized.get(), keep.data());
     pca_res.pcs.adjointInPlace();
+    declare(start);
 
     // Building the nearest neighbor index.
+    std::cout << "Building the neighbor index... " << std::flush;
+    start = std::chrono::high_resolution_clock::now();
     std::unique_ptr<knncolle::Base<> > ptr;
     if (approx) {
         ptr.reset(new knncolle::AnnoyEuclidean<>(npcs, pca_res.pcs.cols(), pca_res.pcs.data()));
     } else {
         ptr.reset(new knncolle::VpTreeEuclidean<>(npcs, pca_res.pcs.cols(), pca_res.pcs.data()));
     }
+    declare(start);
 
     // Finding all the neighbors.
+    std::cout << "Finding neighbors for clustering... " << std::flush;
+    start = std::chrono::high_resolution_clock::now();
     auto snn_nns = knncolle::find_nearest_neighbors_index_only<int>(ptr.get(), snn_neighbors);
+    declare(start);
+
+    std::cout << "Finding neighbors for t-SNE... " << std::flush;
+    start = std::chrono::high_resolution_clock::now();
     auto tsne_nns = knncolle::find_nearest_neighbors<int, double>(ptr.get(), qdtsne::perplexity_to_k(tsne_perplexity));
+    declare(start);
+
+    std::cout << "Finding neighbors for UMAP... " << std::flush;
+    start = std::chrono::high_resolution_clock::now();
     auto umap_nns = knncolle::find_nearest_neighbors<int, float>(ptr.get(), umap_neighbors);
+    declare(start);
 
     // Running all remaining steps in parallel threads.
     std::vector<double> tsne_output = qdtsne::initialize_random<>(ptr->nobs());
@@ -154,19 +199,48 @@ int main(int argc, char* argv []) {
     std::vector<int> best_clustering;
     scran::ScoreMarkers::Results<double> marker_res;
 
+    std::mutex cout_lock; 
+
     std::thread first([&]() -> void {
+        auto start = std::chrono::high_resolution_clock::now();
         auto snn_graph = scran::BuildSNNGraph().set_weighting_scheme(snn_scheme).run(snn_nns);
+        cout_lock.lock();
+        std::cout << "SNN graph construction... " << std::flush;
+        declare(start);
+        cout_lock.unlock();
+
+        start = std::chrono::high_resolution_clock::now();
         auto clust_res = scran::ClusterSNNGraphMultiLevel().set_resolution(snn_res).run(snn_nns.size(), snn_graph);
+        cout_lock.lock();
+        std::cout << "Multi-level clustering... " << std::flush;
+        declare(start);
+        cout_lock.unlock();
+
+        start = std::chrono::high_resolution_clock::now();
         best_clustering = clust_res.membership[clust_res.max];
         marker_res = scran::ScoreMarkers().run(normalized.get(), best_clustering.data());
+        cout_lock.lock();
+        std::cout << "Marker detection... " << std::flush;
+        declare(start);
+        cout_lock.unlock();
     });
 
     std::thread second([&]() -> void {
+        auto start = std::chrono::high_resolution_clock::now();
         qdtsne::Tsne<>().set_perplexity(tsne_perplexity).set_max_iter(tsne_iterations).run(tsne_nns, tsne_output.data());
+        cout_lock.lock();
+        std::cout << "t-SNE calculation... " << std::flush;
+        declare(start);
+        cout_lock.unlock();
     });
 
     std::thread third([&]() -> void {
+        auto start = std::chrono::high_resolution_clock::now();
         umappp::Umap<float>().set_min_dist(umap_mindist).set_num_epochs(umap_epochs).run(umap_nns, 2, umap_output.data());
+        cout_lock.lock();
+        std::cout << "UMAP calculation... " << std::flush;
+        declare(start);
+        cout_lock.unlock();
     });
 
     first.join();
